6f7cca29d20e839f226e7a9b6bc10712489e1027
Alex Betancourt <alex@Alexs-MacBook-Pro.local>
2022-03-22 18:11:48 -0400

    removing variable

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,5 +1,4 @@
 const router = require("express").Router();
-const res = require("express/lib/response");
 const  db = require("../../data/db.json");
 const fs = require('fs');
 const path = require('path');
